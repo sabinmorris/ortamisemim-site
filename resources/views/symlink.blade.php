7bdbca2e03e767f3ffa25859159685009011eff8
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2025-03-12 13:12:30 +0300

    add symlink

---
+++ resources/views/symlink.blade.php
@@ -0,0 +1,5 @@
+Route::get('/storage-link', function(){
+    $storagFolder = storage_path('app/public');
+    $linkFolder = $_SERVER['DOCUMENT_ROOT']. '/storage';
+    symlink($storagFolder, $linkFolder);
+});
