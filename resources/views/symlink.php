c1f3e7362aef1b6b54bfc4ea1ef89de99af70a93
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2025-03-18 13:26:42 +0300

    add so change in symlink

--- resources/views/symlink.php
+++ resources/views/symlink.php
@@ -1,6 +1,9 @@
 
-Route::get('/storage-link', function(){
+
+<?php
     $storagFolder = storage_path('app/public');
     $linkFolder = $_SERVER['DOCUMENT_ROOT']. '/storage';
     symlink($storagFolder, $linkFolder);
-});
+    echo 'success';
+
+?>
