c28d04102857100abbf08bd53b4e120f2385b3e7
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2025-03-18 13:33:12 +0300

    add new symlink

--- resources/views/symlink.php
+++ resources/views/symlink.php
@@ -1,9 +1,11 @@
 
 
 <?php
-    $storagFolder = storage_path('app/public');
-    $linkFolder = $_SERVER['DOCUMENT_ROOT']. '/storage';
-    symlink($storagFolder, $linkFolder);
-    echo 'success';
+    // $storagFolder = storage_path('app/public');
+    // $linkFolder = $_SERVER['DOCUMENT_ROOT']. '/storage';
+    // symlink($storagFolder, $linkFolder);
+    
 
+    symlink('/home/tamisemi/ortamisemim-web/storage/app/public', '/home/tamisemi/public_html/ortamisemim-web/storage');
+    echo 'success';
 ?>
