06d7c04e46b607e4ab1aa0eeb084f6d2bd9fe8ab
Elias morris <30745358+eliasmorris@users.noreply.github.com>
2025-04-17 15:11:43 +0300

    add update button

--- resources/views/admin/pictures/edit.blade.php
+++ resources/views/admin/pictures/edit.blade.php
@@ -77,7 +77,7 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Save') }}
+                                    {{ __('Update') }}
                                 </button>
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                             </div>
