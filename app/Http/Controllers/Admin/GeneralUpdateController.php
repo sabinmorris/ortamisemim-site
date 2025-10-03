a6768d9a0fdd6ce70776022c8fa34e551525a2b7
Elias morris <sabinmorris8@gmail.com>
2025-10-03 12:16:37 +0300

    add some changes

--- app/Http/Controllers/Admin/GeneralUpdateController.php
+++ app/Http/Controllers/Admin/GeneralUpdateController.php
@@ -60,8 +60,8 @@ class GeneralUpdateController extends Controller
         $this->validate($request, [
             'tittlee' => ['required', 'string', 'max:255'],
             'captionn' => ['required', 'string', 'max:255'],
-            //'slide_imagee' => 'mimes:webp|nullable|max:5120' // max 5120kb
              'statuss' => 'required',
+             'slide_imagee' => 'mimes:webp|nullable|max:5120' // max 5120kb
         ]);
 
         $slideInfo = Slide::findOrFail($request->slideid);
