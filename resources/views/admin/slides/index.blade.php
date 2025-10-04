4211576a19bedc49d9c6717fb7fd9c8ac3b606ee
Elias morris <sabinmorris8@gmail.com>
2025-10-04 12:44:00 +0300

    add validation error in ajax slide

--- resources/views/admin/slides/index.blade.php
+++ resources/views/admin/slides/index.blade.php
@@ -282,6 +282,19 @@
                             document.location.reload();
                         }, 2000); // 2000 milliseconds = 2 seconds
 
+                    },
+                    error: function(xhr) {
+
+                        if (xhr.status === 422) {
+                            // Laravel validation errors
+                            let errors = xhr.responseJSON.errors;
+                            $.each(errors, function(field, messages) {
+                                toastr.error(messages[0], field.toUpperCase() + ' Error');
+                            });
+                        } else {
+                            toastr.error(xhr.responseJSON?.message || 'Unexpected error occurred.');
+                        }
+
                     }
                 });
             }
